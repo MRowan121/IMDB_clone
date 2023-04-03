dc056e89c475f16c86759404a9263dbf46f7dfb7
Matt Rowan <matt.rowan121@gmail.com>
2023-04-02 22:30:54 -0400

    Add remaining header icon components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image"
 import HeaderIcon from "./HeaderIcon"
+import { 
+    FaHome,
+    FaUser, 
+    FaPhoneAlt, 
+    FaInfoCircle
+} from 'react-icons/fa'
 
 const Header = () => {
     return (
         <div>
             <div className="">
-                <HeaderIcon />
+                <HeaderIcon Icon={FaHome} title="HOME" />
+                <HeaderIcon Icon={FaUser} title="ACCOUNT" />
+                <HeaderIcon Icon={FaPhoneAlt} title="CONTACT" />
+                <HeaderIcon Icon={FaInfoCircle} title="ABOUT" />
             </div>
             <Image 
                 src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
