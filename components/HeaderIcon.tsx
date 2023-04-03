6836b5069ed4ca234adc0d0ec77a364adeaadc20
Matt Rowan <matt.rowan121@gmail.com>
2023-04-02 22:26:31 -0400

    Create initial buildout of header icon component

--- components/HeaderIcon.tsx
+++ components/HeaderIcon.tsx
@@ -1,7 +1,16 @@
-const HeaderIcon = () => {
+import React from "react";
+import { IconType } from "react-icons";
+
+interface MyProps {
+    Icon: IconType,
+    title: string
+}
+
+const HeaderIcon: React.FC<MyProps> = ({ Icon, title }) => {
     return (
         <div>
-
+            <Icon className="text-3xl" />
+            <p>{title}</p>
         </div>
     )
 }
