34f672910a345df7d661c3faad3b1d8166efc8e5
Matt Rowan <matt.rowan121@gmail.com>
2023-04-07 16:36:39 -0400

    Add links to home with header images

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,23 +6,28 @@ import {
     FaPhoneAlt, 
     FaInfoCircle
 } from 'react-icons/fa'
+import Link from "next/link"
 
 const Header = () => {
     return (
         <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
             <div className="flex">
-                <HeaderIcon Icon={FaHome} title="HOME" />
+                <Link href={'/'} >
+                    <HeaderIcon Icon={FaHome} title="HOME" />
+                </Link>
                 <HeaderIcon Icon={FaUser} title="ACCOUNT" />
                 <HeaderIcon Icon={FaPhoneAlt} title="CONTACT" />
                 <HeaderIcon Icon={FaInfoCircle} title="ABOUT" />
             </div>
-            <Image 
-                src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
-                alt="imdb-logo"
-                width={100}
-                height={100}
-                className="cursor-pointer active:brightness-110"
-            />
+            <Link href={'/'} >
+                <Image 
+                    src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
+                    alt="imdb-logo"
+                    width={100}
+                    height={100}
+                    className="cursor-pointer active:brightness-110"
+                />
+            </Link>
         </div>
     )
 }
